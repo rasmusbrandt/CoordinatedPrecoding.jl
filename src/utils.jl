f5565316339228328d6164b93db284ac03adf4af
Rasmus Brandt <rasmusb85@gmail.com>
2015-02-03 22:38:59 +0100

    Generalized clean_simulation_params_for_jld
    - Now handles if not all parameters are set.

--- src/utils.jl
+++ src/utils.jl
@@ -1,36 +1,44 @@
 ##########################################################################
 # General utilities
 function clean_simulation_params_for_jld(simulation_params)
+    cleaned_simulation_params = copy(simulation_params)
+
     # Precoding method function pointers
-    cleaned_precoding_methods = Array(ASCIIString, 0)
-    for method in simulation_params["precoding_methods"]
-        push!(cleaned_precoding_methods, string(method))
+    if haskey(simulation_params, "precoding_methods")
+        cleaned_precoding_methods = Array(ASCIIString, 0)
+        for method in simulation_params["precoding_methods"]
+            push!(cleaned_precoding_methods, string(method))
+        end
+        cleaned_simulation_params["precoding_methods"] = cleaned_precoding_methods
     end
 
     # Precoding params function pointers
-    cleaned_aux_precoding_params = Dict{ASCIIString, Any}()
-    for (key, val) in simulation_params["aux_precoding_params"]
-        if isa(val, Function)
-            cleaned_aux_precoding_params[key] = string(val)
-        else
-            cleaned_aux_precoding_params[key] = val
+    if haskey(simulation_params, "aux_precoding_params")
+        cleaned_aux_precoding_params = Dict{ASCIIString, Any}()
+        for (key, val) in simulation_params["aux_precoding_params"]
+            if isa(val, Function)
+                cleaned_aux_precoding_params[key] = string(val)
+            else
+                cleaned_aux_precoding_params[key] = val
+            end
         end
+        cleaned_simulation_params["aux_precoding_params"] = cleaned_aux_precoding_params
     end
 
     # Independent variable function pointer
-    cleaned_independent_variable = (string(simulation_params["independent_variable"][1]), simulation_params["independent_variable"][2])
-
-    # Auxiliary independent variable function pointers
-    cleaned_aux_independent_variables = Array((ASCIIString, Any), 0)
-    for aux_idp in simulation_params["aux_independent_variables"]
-        push!(cleaned_aux_independent_variables, (string(aux_idp[1]), aux_idp[2]))
+    if haskey(simulation_params, "independent_variable")
+        cleaned_independent_variable = (string(simulation_params["independent_variable"][1]), simulation_params["independent_variable"][2])
+        cleaned_simulation_params["independent_variable"] = cleaned_independent_variable
     end
 
-    cleaned_simulation_params = copy(simulation_params)
-    cleaned_simulation_params["precoding_methods"] = cleaned_precoding_methods
-    cleaned_simulation_params["aux_precoding_params"] = cleaned_aux_precoding_params
-    cleaned_simulation_params["independent_variable"] = cleaned_independent_variable
-    cleaned_simulation_params["aux_independent_variables"] = cleaned_aux_independent_variables
+    # Auxiliary independent variable function pointers
+    if haskey(simulation_params, "aux_independent_variables")
+        cleaned_aux_independent_variables = Array((ASCIIString, Any), 0)
+        for aux_idp in simulation_params["aux_independent_variables"]
+            push!(cleaned_aux_independent_variables, (string(aux_idp[1]), aux_idp[2]))
+        end
+        cleaned_simulation_params["aux_independent_variables"] = cleaned_aux_independent_variables
+    end
 
     return cleaned_simulation_params
 end
