6157b1ba1c8de56c8a0cfa2f5876aa3afa349117
Rasmus Brandt <rasmusb85@gmail.com>
2015-08-21 10:35:10 +0200

    Let Razaviyayn2013_MinMaxWMMSE bail if Gurobi.jl is missing.

--- src/precoding/Razaviyayn2013_MinMaxWMMSE.jl
+++ src/precoding/Razaviyayn2013_MinMaxWMMSE.jl
@@ -30,53 +30,58 @@ function Razaviyayn2013_MinMaxWMMSE(channel, network)
         Array(Hermitian{Complex128}, K),
         initial_precoders(channel, Ps, sigma2s, ds, assignment, aux_params))
     objective = Float64[]
-    logdet_rates = Array(Float64, K, max_d, aux_params["max_iters"])
-    MMSE_rates = Array(Float64, K, max_d, aux_params["max_iters"])
-    weighted_logdet_rates = Array(Float64, K, max_d, aux_params["max_iters"])
-    weighted_MMSE_rates = Array(Float64, K, max_d, aux_params["max_iters"])
-    allocated_power = Array(Float64, K, max_d, aux_params["max_iters"])
+    logdet_rates = zeros(Float64, K, max_d, aux_params["max_iters"])
+    MMSE_rates = zeros(Float64, K, max_d, aux_params["max_iters"])
+    weighted_logdet_rates = zeros(Float64, K, max_d, aux_params["max_iters"])
+    weighted_MMSE_rates = zeros(Float64, K, max_d, aux_params["max_iters"])
+    allocated_power = zeros(Float64, K, max_d, aux_params["max_iters"])
 
-    iters = 0; conv_crit = Inf
-    while iters < aux_params["max_iters"]
-        update_MSs!(state, channel, sigma2s, assignment)
-        iters += 1
+    if Pkg.installed("Gurobi") == nothing
+        iters = 1; objective = 0.
+        Lumberjack.warn("Razaviyayn2013_MinMaxWMMSE exiting because Gurobi.jl is not installed.")
+    else
+        iters = 0; conv_crit = Inf
+        while iters < aux_params["max_iters"]
+            update_MSs!(state, channel, sigma2s, assignment)
+            iters += 1
 
-        # Results after this iteration
-        logdet_rates[:,:,iters] = calculate_logdet_rates(state)
-        push!(objective, minimum(sum(logdet_rates[:,:,iters], 2)))
-        MMSE_rates[:,:,iters] = calculate_MMSE_rates(state)
-        weighted_logdet_rates[:,:,iters] = calculate_weighted_logdet_rates(state, alphas)
-        weighted_MMSE_rates[:,:,iters] = calculate_weighted_MMSE_rates(state, alphas)
-        allocated_power[:,:,iters] = calculate_allocated_power(state)
+            # Results after this iteration
+            logdet_rates[:,:,iters] = calculate_logdet_rates(state)
+            push!(objective, minimum(sum(logdet_rates[:,:,iters], 2)))
+            MMSE_rates[:,:,iters] = calculate_MMSE_rates(state)
+            weighted_logdet_rates[:,:,iters] = calculate_weighted_logdet_rates(state, alphas)
+            weighted_MMSE_rates[:,:,iters] = calculate_weighted_MMSE_rates(state, alphas)
+            allocated_power[:,:,iters] = calculate_allocated_power(state)
 
-        # Check convergence
-        if iters >= 2
-            conv_crit = abs(objective[end] - objective[end-1])/abs(objective[end-1])
-            if conv_crit < aux_params["stop_crit"]
-                Lumberjack.debug("Razaviyayn2013_MinMaxWMMSE converged.",
-                    [ :no_iters => iters,
-                      :final_objective => objective[end],
-                      :conv_crit => conv_crit,
-                      :stop_crit => aux_params["stop_crit"],
-                      :max_iters => aux_params["max_iters"] ]
-                )
-                break
+            # Check convergence
+            if iters >= 2
+                conv_crit = abs(objective[end] - objective[end-1])/abs(objective[end-1])
+                if conv_crit < aux_params["stop_crit"]
+                    Lumberjack.debug("Razaviyayn2013_MinMaxWMMSE converged.",
+                        [ :no_iters => iters,
+                          :final_objective => objective[end],
+                          :conv_crit => conv_crit,
+                          :stop_crit => aux_params["stop_crit"],
+                          :max_iters => aux_params["max_iters"] ]
+                    )
+                    break
+                end
+            end
+
+            # Begin next iteration, unless the loop will end
+            if iters < aux_params["max_iters"]
+                update_BSs!(state, channel, Ps, sigma2s, assignment, aux_params)
             end
         end
-
-        # Begin next iteration, unless the loop will end
-        if iters < aux_params["max_iters"]
-            update_BSs!(state, channel, Ps, sigma2s, assignment, aux_params)
+        if iters == aux_params["max_iters"]
+            Lumberjack.debug("Razaviyayn2013_MinMaxWMMSE did NOT converge.",
+                [ :no_iters => iters,
+                  :final_objective => objective[end],
+                  :conv_crit => conv_crit,
+                  :stop_crit => aux_params["stop_crit"],
+                  :max_iters => aux_params["max_iters"] ]
+            )
         end
-    end
-    if iters == aux_params["max_iters"]
-        Lumberjack.debug("Razaviyayn2013_MinMaxWMMSE did NOT converge.",
-            [ :no_iters => iters,
-              :final_objective => objective[end],
-              :conv_crit => conv_crit,
-              :stop_crit => aux_params["stop_crit"],
-              :max_iters => aux_params["max_iters"] ]
-        )
     end
 
     results = PrecodingResults()
